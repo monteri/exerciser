9d22475a978087402a489541665b2158bf140713
monteri <lansevermore>
2023-09-12 16:23:16 +0300

    feat: support staticfiles

--- conf/urls.py
+++ conf/urls.py
@@ -14,10 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.views.static import serve
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('slack/', include('app.urls')),
 ]
+
+if settings.DEBUG is False:  # Only serve static files from Django in production mode
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
+    ]
